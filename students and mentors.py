10836f5ed3e0da17528e0b0b6f9a18df43723fa4
Kotiev Timur <nick.hoult@mail.ru>
2023-10-02 19:04:21 +0300

    task3_done

--- students and mentors.py
+++ students and mentors.py
@@ -15,11 +15,19 @@ class Student:
             else:
                 lecturer.scores[course] = [score]
         else:
-            return 'Ошибка'          
+            return 'Ошибка'
+
+    def average_grades(self):
+        if self.grades:
+            common_list = sum(self.grades.values(), start=[])
+            return sum(common_list)/len(common_list)
+        elif not self.grades:
+            return "Нет оценок"    
+                  
 
 #task 3    
     def __str__(self):
-        return f'Имя: {self.name} \n Фамилия: {self.surname} \nКурсы в процессе изучения: {self.courses_in_progress} \nЗавершенные курсы: {self.finished_courses}'
+        return f'Имя студента: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за домашние задания: {self.average_grades()} \n Курсы в процессе изучения: {self.courses_in_progress} \nЗавершенные курсы: {self.finished_courses}'
 
 
 class Mentor:
@@ -38,8 +46,11 @@ class Lecturer(Mentor):
         print (f'В словаре у лектора: {self.scores}')    
 #task 3
     def average_score(self):
-        common_list = sum(self.scores.values(), start=[])
-        return sum(common_list)/len(common_list)
+        if self.scores:
+            common_list = sum(self.scores.values(), start=[])
+            return sum(common_list)/len(common_list)
+        elif not self.scores:
+            return "Нет оценок"
 
     def __str__(self):
         return f'Имя лектора: {self.name} \n Фамилия: {self.surname} \n средняя оценка за лекции: {self.average_score()}' 
@@ -64,24 +75,24 @@ class Reviewer(Mentor):
         return f'Имя ревьюера: {self.name} \n Фамилия: {self.surname}'
     
 
-some_student1 = Student('Frodo', 'Baggins', 'Male')
-some_student1.courses_in_progress+= ['Python']
-some_student1.courses_in_progress+= ['Git'] 
-some_student1.finished_courses += ['Введение в программирование']
+some_student = Student('Frodo', 'Baggins', 'Male')
+some_student.courses_in_progress+= ['Python']
+some_student.courses_in_progress+= ['Git'] 
+some_student.finished_courses += ['Введение в программирование']
 
 some_reviewer = Reviewer('Bilbo', 'Baggins')
 some_reviewer.courses_attached += ['Python']
 some_reviewer.courses_attached += ['Git']
-some_reviewer.rate_home_work(some_student1, 'Python', 10)
-some_reviewer.rate_home_work(some_student1, 'Git', 9)
+some_reviewer.rate_home_work(some_student, 'Python', 10)
+some_reviewer.rate_home_work(some_student, 'Git', 9)
 
 some_lecturer1 = Lecturer('Gandalf', 'Gray')
 some_lecturer1.courses_attached += ['Python']
 some_lecturer1.courses_attached += ['Git']
  
-some_student1.rate_lecturer(some_lecturer1, 'Python', 9)
-some_student1.rate_lecturer(some_lecturer1, 'Python', 10)
-some_student1.rate_lecturer(some_lecturer1, 'Git', 8)
+some_student.rate_lecturer(some_lecturer1, 'Python', 9)
+some_student.rate_lecturer(some_lecturer1, 'Python', 10)
+some_student.rate_lecturer(some_lecturer1, 'Git', 8)
 
 some_student1 = Student('Ruoy', 'Eman', 'Male')
 print(some_reviewer)
@@ -91,15 +102,16 @@ print(some_lecturer1)
 some_lecturer1.show_grades()
 
 print()
-print(some_student1)
+print(some_student)
 
 some_student2 = Student('Harry', 'Potter', 'Male')
 some_student2.courses_in_progress+= ['Python']
 some_student2.courses_in_progress+= ['Git']
 some_student2.finished_courses += ['Введение в программирование']
 
-some_lecturer2 = Lecturer('Albus', 'Dumbledore')
-some_lecturer2.courses_attached += ['Python']
-some_lecturer2.courses_attached += ['Git']
+# some_lecturer2 = Lecturer('Albus', 'Dumbledore')
+# some_lecturer2.courses_attached += ['Python']
+# some_lecturer2.courses_attached += ['Git']
 
-student_list= [some_student1,some_student2]
+print(some_student2)
+
