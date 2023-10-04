226f7998e61b037fe89949f617bdd48ed4fed4d9
Timur Kotiev <144617836+CatGaster@users.noreply.github.com>
2023-10-04 17:59:47 +0300

    Update students and mentors.py
    Небольшое улучшения читаемости кода

--- students and mentors.py
+++ students and mentors.py
@@ -6,7 +6,6 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
     #task 2
     def rate_lecturer(self, lecturer, course, score):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
@@ -22,9 +21,7 @@ class Student:
             common_list = sum(self.grades.values(), start=[])
             return sum(common_list)/len(common_list)
         elif not self.grades:
-            return "Нет оценок"    
-                  
-
+            return "Нет оценок"                      
 #task 3    
     def __str__(self):
         return f'Имя студента: {self.name} \n Фамилия: {self.surname} \n Средняя оценка за домашние задания: {self.average_grades()} \n Курсы в процессе изучения: {self.courses_in_progress} \nЗавершенные курсы: {self.finished_courses}'
@@ -51,7 +48,7 @@ class Lecturer(Mentor):
             return sum(common_list)/len(common_list)
         elif not self.scores:
             return "Нет оценок"
-
+            
     def __str__(self):
         return f'Имя лектора: {self.name} \n Фамилия: {self.surname} \n средняя оценка за лекции: {self.average_score()}' 
 
@@ -59,7 +56,6 @@ class Lecturer(Mentor):
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname) 
-
 #task 2
     def rate_home_work(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached  and course in student.courses_in_progress:
@@ -68,12 +64,11 @@ class Reviewer(Mentor):
             else:
                 student.grades[course] = [grade]
         else:
-            return "Ошибка"     
-        
+            return "Ошибка"            
 #task 3
     def __str__(self):
         return f'Имя ревьюера: {self.name} \n Фамилия: {self.surname}'
-    
+
 
 some_student1 = Student('Frodo', 'Baggins', 'Male')
 some_student1.courses_in_progress+= ['Python']
@@ -94,13 +89,9 @@ some_student1.rate_lecturer(some_lecturer1, 'Python', 9)
 some_student1.rate_lecturer(some_lecturer1, 'Python', 10)
 some_student1.rate_lecturer(some_lecturer1, 'Git', 8)
 
-print(some_reviewer1)
-print()
-print(some_lecturer1)
-some_lecturer1.show_grades()
-print()
-print(some_student1)
-print()
+print(f"{some_reviewer1}\n")
+print(f"{some_lecturer1}\n")
+print(f"{some_student1}\n")
 
 some_student2 = Student('Harry', 'Potter', 'Male')
 some_student2.courses_in_progress+= ['Python']
@@ -121,12 +112,9 @@ some_student2.rate_lecturer(some_lecturer2, 'Python', 9)
 some_student2.rate_lecturer(some_lecturer2, 'Python', 10)
 some_student2.rate_lecturer(some_lecturer2, 'Git', 10)
 
-print(some_reviewer2)
-print()
-print(some_lecturer2)
-print()
-print(some_student2)
-print()
+print(f"{some_reviewer2}\n")
+print(f"{some_lecturer2}\n")
+print(f"{some_student2}\n")
 
 students_list= [some_student1,some_student2]
 
